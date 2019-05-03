8d026d6f33024f59c595061baffd5a11a8830b8e
ryan <ryan@whiteroomautmation.com>
2019-05-03 22:16:16 +0000

    v 1

--- WW_Hist_Data_Export/Form1.cs
+++ WW_Hist_Data_Export/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Data.SqlClient;
 
 namespace WW_Hist_Data_Export
 {
@@ -16,5 +19,45 @@ namespace WW_Hist_Data_Export
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sql_connection;
+
+            SqlCommand sql_command;
+            int iCount;
+            int iIndex = 0;
+
+
+            DataTable dt = new DataTable();
+            string szSQLConnectionInfo;
+            szSQLConnectionInfo = "Data Source=localhost;Initial Catalog=Runtime;Trusted_Connection=true";
+            sql_connection = new SqlConnection(szSQLConnectionInfo);
+            sql_command = new SqlCommand("SELECT [TagName] FROM[dbo].[Tag] where[TagName] not like 'sys%'", sql_connection);
+            sql_command.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(sql_command);
+            da.Fill(dt);
+            iCount = dt.Rows.Count;
+
+
+            List<string> szTagLists = new List<string>();
+            string szTagList = "";
+
+
+            foreach (DataRow row in dt.Rows)
+            {
+                szTagList = szTagList +  "[" + row[0].ToString() + "],";
+                iIndex = iIndex + 1;
+                if (iIndex == 100)
+                {
+                    szTagLists.Add(szTagList);
+                    szTagList = "";
+                    iIndex = 0;
+                }
+
+
+            }
+        }
+       
     }
 }
